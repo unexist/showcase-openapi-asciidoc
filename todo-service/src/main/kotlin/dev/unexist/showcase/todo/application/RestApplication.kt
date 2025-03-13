2e0dec6a83d4e3e3f9b361823926251618270b4a
Christoph Kappel <christoph@unexist.dev>
2025-03-13 13:55:53 +0100

    Fixed config handling due to update to 3.x

--- todo-service/src/main/kotlin/dev/unexist/showcase/todo/application/RestApplication.kt
+++ todo-service/src/main/kotlin/dev/unexist/showcase/todo/application/RestApplication.kt
@@ -15,22 +15,28 @@ import com.orbitz.consul.Consul
 import com.orbitz.consul.ConsulException
 import com.orbitz.consul.model.agent.ImmutableRegistration
 import io.github.oshai.kotlinlogging.KotlinLogging
-import io.ktor.server.engine.commandLineEnvironment
+import io.ktor.server.engine.CommandLineConfig
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.loadCommonConfiguration
 import io.ktor.server.netty.Netty
 import java.net.ConnectException
 
 fun main(args: Array<String>) {
     val logger = KotlinLogging.logger {}
-
-    val server = embeddedServer(Netty, commandLineEnvironment(args))
+    val server = embeddedServer(
+        factory = Netty,
+        configure = {
+            val cliConfig = CommandLineConfig(args)
+            takeFrom(cliConfig.engineConfig)
+            loadCommonConfiguration(cliConfig.rootConfig.environment.config)
+        })
 
     /* Consul */
     val consulService = ImmutableRegistration.builder()
-            .id("todo-${server.environment.connectors[0].port}")
+            .id("todo-${server.engineConfig.connectors[0].port}")
             .name("todo-service")
             .address("localhost")
-            .port(server.environment.connectors[0].port)
+            .port(server.engineConfig.connectors[0].port)
             .build()
 
     try {
