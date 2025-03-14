5371a0e1a85a64915118223e965fa9daecc6b721
Christoph Kappel <christoph@unexist.dev>
2025-03-14 14:43:24 +0100

    Updated routes

--- todo-service/src/main/kotlin/dev/unexist/showcase/todo/adapter/TodoRoute.kt
+++ todo-service/src/main/kotlin/dev/unexist/showcase/todo/adapter/TodoRoute.kt
@@ -14,7 +14,6 @@ package dev.unexist.showcase.todo.adapter
 import dev.unexist.showcase.todo.domain.todo.Todo
 import dev.unexist.showcase.todo.domain.todo.TodoBase
 import dev.unexist.showcase.todo.domain.todo.TodoService
-import dev.unexist.showcase.todo.infrastructure.persistence.TodoListRepository
 import io.github.smiley4.ktoropenapi.delete
 import io.github.smiley4.ktoropenapi.get
 import io.github.smiley4.ktoropenapi.put
@@ -172,21 +171,8 @@ fun Routing.todo() {
                             value = 1L
                         }
                     }
-                    response {
-                        code(HttpStatusCode.OK) {
-                            body<Todo>{
-                                description = "The found todo with the given id"
-                                example("Todo") {
-                                    value = Todo(
-                                        title = "Title",
-                                        description = "Description",
-                                        done = false,
-                                        dueDate = null
-                                    )
-                                }
-                            }
-                        }
-                    }
+                }
+                response {
                     code(HttpStatusCode.OK) {
                         body<Todo>{
                             description = "The found todo with the given id"
@@ -200,6 +186,45 @@ fun Routing.todo() {
                             }
                         }
                     }
+                    code(HttpStatusCode.NoContent) {
+                        description = "Todo updated"
+                    }
+                    code(HttpStatusCode.NotFound) {
+                        description = "Todo not found"
+                    }
+                    code(HttpStatusCode.InternalServerError) {
+                        description = "Internal server error"
+                    }
+                }
+            }) {
+                val id: String? = call.parameters["id"]
+                val todoBase: TodoBase = call.receive()
+
+                if (null != id) {
+                    if (todoService.update(id.toInt(), todoBase)) {
+                        call.respond(HttpStatusCode.NoContent)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound)
+                    }
+                }
+            }
+
+            delete({
+                operationId = "deleteTodoById"
+                description = "Delete todo by a single ID"
+                request {
+                    pathParameter<Long>("id") {
+                        description = "Id of todo to fetch"
+                        required = true
+                        example("default") {
+                            value = 1L
+                        }
+                    }
+                }
+                response {
+                    code(HttpStatusCode.NoContent) {
+                        description = "Todo deleted"
+                    }
                     code(HttpStatusCode.NotFound) {
                         description = "Todo not found"
                     }
@@ -210,32 +235,13 @@ fun Routing.todo() {
             }) {
                 val id: String? = call.parameters["id"]
 
-                if (null != id)
-                    call.respond(message = TodoListRepository.getAll()
-                        .first { it.id == id.toInt() })
-            }
-
-            delete({
-                operationId = "deletePet"
-                description = "deletes a single pet based on the supplied ID"
-                request {
-                    pathParameter<Long>("id") {
-                        description = "Id of pet to delete"
-                        required = true
-                        example("default") {
-                            value = 123L
-                        }
+                if (null != id) {
+                    if (todoService.delete(id.toInt())) {
+                        call.respond(HttpStatusCode.NoContent)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound)
                     }
                 }
-                response {
-                    code(HttpStatusCode.NoContent) {
-                        description = "the pet was successfully deleted"
-                    }
-                    code(HttpStatusCode.NotFound) {
-                        description = "the pet with the given id was not found"
-                    }
-                }
-            }) {
                 call.respond(HttpStatusCode.NotImplemented, Unit)
             }
         }
@@ -251,127 +257,7 @@ fun Routing.todo() {
 
     route("redoc") {
         redoc("/api.json") {
-            // Add configuration for this ReDoc "instance" here.
+            hideLoading = true
         }
     }
 }
-
-    /*
-        @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Create new todo")
-    @Tag(name = "Todo")
-    @APIResponses(
-        APIResponse(responseCode = "201", description = "Todo created"),
-        APIResponse(responseCode = "406", description = "Bad data"),
-        APIResponse(responseCode = "500", description = "Server error")
-    )
-    fun create(todoBase: TodoBase, @Context uriInfo: UriInfo): Response {
-        val response: Response.ResponseBuilder
-
-        val todo: Optional<Todo> = this.todoService.create(todoBase)
-
-        if (todo.isPresent) {
-            val uri: URI = uriInfo.absolutePathBuilder
-                    .path(todo.get().id.toString())
-                    .build();
-
-            response = Response.created(uri).entity(todo.get())
-        } else {
-            response = Response.status(Response.Status.NOT_ACCEPTABLE);
-        }
-
-        return response.build();
-    }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Get all todos")
-    @Tag(name = "Todo")
-    @APIResponses(
-        APIResponse(responseCode = "200", description = "List of todo", content = [
-            Content(schema = Schema(type = SchemaType.ARRAY, implementation = Todo::class))]),
-        APIResponse(responseCode = "204", description = "Nothing found"),
-        APIResponse(responseCode = "500", description = "Server error")
-    )
-    fun getAll(): Response {
-        val todoList: List<Todo> = this.todoService.getAll()
-
-        val response: Response.ResponseBuilder
-
-        if (todoList.isEmpty()) {
-            response = Response.noContent();
-        } else {
-            response = Response.ok(Entity.json(todoList));
-        }
-
-        return response.build();
-    }
-
-    @GET
-    @Path("{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Get todo by id")
-    @Tag(name = "Todo")
-    @APIResponses(
-        APIResponse(responseCode = "200", description = "Todo found", content = [
-            (Content(schema = Schema(implementation = Todo::class)))]),
-        APIResponse(responseCode = "404", description = "Todo not found"),
-        APIResponse(responseCode = "500", description = "Server error")
-    )
-    fun findById(@PathParam("id") id: Int): Response {
-        val result: Optional<Todo> = this.todoService.findById(id)
-
-        val response: Response.ResponseBuilder
-
-        if (result.isPresent) {
-            response = Response.ok(Entity.json(result.get()));
-        } else {
-            response = Response.status(Response.Status.NOT_FOUND);
-        }
-
-        return response.build();
-    }
-
-    @PUT
-    @Path("{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Update todo by id")
-    @Tag(name = "Todo")
-    @APIResponses(
-        APIResponse(responseCode = "204", description = "Todo updated"),
-        APIResponse(responseCode = "404", description = "Todo not found"),
-        APIResponse(responseCode = "500", description = "Server error")
-    )
-    fun update(@PathParam("id") id: Int, base: TodoBase): Response {
-        val response: Response.ResponseBuilder
-
-        if (this.todoService.update(id, base)) {
-            response = Response.noContent();
-        } else {
-            response = Response.status(Response.Status.NOT_FOUND);
-        }
-
-        return response.build();
-    }
-
-    @DELETE
-    @Path("{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Delete todo by id")
-    @Tag(name = "Todo")
-    fun delete(@PathParam("id") id: Int, base: TodoBase): Response {
-        val response: Response.ResponseBuilder
-
-        if (this.todoService.delete(id)) {
-            response = Response.noContent();
-        } else {
-            response = Response.status(Response.Status.NOT_FOUND);
-        }
-
-        return response.build();
-    }
-     */
