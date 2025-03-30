1a79e029c2a986cbf0d19a2fa4edc559ed0f4071
Christoph Kappel <christoph@unexist.dev>
2025-03-30 14:47:16 +0200

    Added app description and re-rolled openapi spec

--- todo-service/src/main/kotlin/dev/unexist/showcase/todo/application/ApplicationConfig.kt
+++ todo-service/src/main/kotlin/dev/unexist/showcase/todo/application/ApplicationConfig.kt
@@ -109,6 +109,7 @@ fun Application.appModule() {
         info {
             title = "OpenAPI for todo-service"
             version = "0.1"
+            description = "Simple todo service"
             contact {
                 name = "Christoph Kappel"
                 url = "https://unexist.dev"
